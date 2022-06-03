2bd326c7367d6009c1abec3a782ff6ee63bf2dfa
Ilkogamer626 <tomasko.ilias.ti@gmail.com>
2022-06-03 22:26:32 +0200

    Downloading of youtube video done

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+import requests
+import json
+from types import SimpleNamespace
+import youtube_dl
+from slugify import slugify
+
 
 root = tk.Tk()
 root.title('Give me!')
@@ -19,7 +25,7 @@ message = ttk.Label(root, text="Hello world")
 message.pack()
 
 # getting input
-text = tk.StringVar(value='Song name')
+text = tk.StringVar(value='')
 inputField = ttk.Entry(root, textvariable=text)
 inputField.pack()
 
@@ -29,9 +35,46 @@ inputField.pack()
 def buttonClicked():
     writtenText = text.get()
     message.config(text=writtenText)
+    if(writtenText):
+        toBeAddedIntoUrl = slugify(writtenText, separator='%20')
+        print(toBeAddedIntoUrl)
+        httpUrl = f'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q={toBeAddedIntoUrl}&key=AIzaSyCf6bBW5Ujtp1vOF0ycQO6cKtYSUBMa5ps'
+        print(httpUrl)
+        response = requests.get(
+            httpUrl)
+        responseObject = response.json()
+        # I somehow need to transform dictionary or whatever to json I guess
+        items = responseObject["items"]
+        firstItem = items[0]
+        firstItemId = firstItem['id']
+        youtubeId = firstItemId['videoId']
+
+        videoId = ttk.Label(root, text=youtubeId)
+        videoId.pack()
+
+        # downloading the video
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        print(youtubeId)
+        urlToDownload = f'https://youtu.be/{youtubeId}'
+        print(urlToDownload)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([urlToDownload])
 
 
 button = ttk.Button(root, text='Read', command=buttonClicked)
 button.pack()
 
+
 root.mainloop()
+
+
+# https://www.youtube.com/results?search_query=big+iron
+# GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=big%20iron&key=[YOUR_API_KEY] HTTP/1.1
+# AIzaSyCf6bBW5Ujtp1vOF0ycQO6cKtYSUBMa5ps
